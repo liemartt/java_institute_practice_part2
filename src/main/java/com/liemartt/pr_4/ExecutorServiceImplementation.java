823e590206a15dabb93f822c1cc629abc9f152fc
Artem_Limonov <liemartt@yandex.ru>
2024-02-27 20:44:58 +0300

    feat: implemented 4 practice

---
+++ src/main/java/com/liemartt/pr_4/ExecutorServiceImplementation.java
@@ -0,0 +1,84 @@
+package com.liemartt.pr_4;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class ExecutorServiceImplementation implements ExecutorService {
+    private final ExecutorService executorService;
+
+    public ExecutorServiceImplementation(int numberOfThreads) {
+        this.executorService = Executors.newFixedThreadPool(numberOfThreads);
+    }
+
+    @Override
+    public void shutdown() {
+        executorService.shutdown();
+    }
+
+    //	Остановка всех активно выполняемых задач, остановка обработки ожидающих задач, возвращение списка задач, ожидающих выполнения
+    @Override
+    public List<Runnable> shutdownNow() {
+        return executorService.shutdownNow();
+    }
+
+    @Override
+    public boolean isShutdown() {
+        return executorService.isShutdown();
+    }
+
+    @Override
+    public boolean isTerminated() {
+        return executorService.isTerminated();
+    }
+
+    //Блокировка до тех пор, пока все задачи не завершат выполнение после запроса на завершение работы или
+    // пока не наступит тайм-аут или не будет прерван текущий поток, в зависимости от того, что произойдет раньше
+    @Override
+    public boolean awaitTermination(long timeout, TimeUnit unit) throws InterruptedException {
+        return executorService.awaitTermination(timeout,unit);
+    }
+
+    //Завершение выполнения задачи, возвращающей результат в виде объекта Future
+    @Override
+    public <T> Future<T> submit(Callable<T> task) {
+        return executorService.submit(task);
+    }
+
+    @Override
+    public <T> Future<T> submit(Runnable task, T result) {
+        return executorService.submit(task,result);
+    }
+
+    @Override
+    public Future<?> submit(Runnable task) {
+        return executorService.submit(task);
+    }
+
+    //Выполнение задач с возвращением списка задач с их статусом и результатами завершения
+    @Override
+    public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks) throws InterruptedException {
+        return executorService.invokeAll(tasks);
+    }
+
+    @Override
+    public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException {
+        return executorService.invokeAll(tasks, timeout,unit);
+    }
+
+    @Override
+    public <T> T invokeAny(Collection<? extends Callable<T>> tasks) throws InterruptedException, ExecutionException {
+        return executorService.invokeAny(tasks);
+    }
+
+    @Override
+    public <T> T invokeAny(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+        return executorService.invokeAny(tasks,timeout,unit);
+    }
+
+    @Override
+    public void execute(Runnable command) {
+
+        executorService.execute(command);
+    }
+}
